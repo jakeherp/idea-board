5b3f80ca1c53d93d215d72d1f0eb9df620e48f08
Jacob Herper <jacob@herper.uk>
2019-08-25 00:08:32 +0100

    Updates

---
+++ precache-manifest.2afaed5510f2fb9bff0264f2bc76796a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5e70daa4bdc1046bb07f5183964ac4",
+    "url": "/idea-board/index.html"
+  },
+  {
+    "revision": "f9f48b4dc706de0a6a05",
+    "url": "/idea-board/static/js/2.7ac3661b.chunk.js"
+  },
+  {
+    "revision": "06e9eb35efed6198a0e0",
+    "url": "/idea-board/static/js/main.917e4450.chunk.js"
+  },
+  {
+    "revision": "4c08c4186428646effe7",
+    "url": "/idea-board/static/js/runtime~main.d1799a13.js"
+  }
+]);
